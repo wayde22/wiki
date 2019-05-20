eeb4ad92281a3034a8a290cf51c1b24541022646
Wade Stevens <wstevens22@protonmail.com>
2019-05-20 10:08:10 -0500

    Update quicktempnote.js

--- quicktempnote.js
+++ quicktempnote.js
@@ -1,1 +1,8 @@
 // Quick Temperary Notes
+
+## React
+keys should be unique
+You can call the import function last
+name of the main app should be the same as the name of the app
+look up destructuring. It is easier than doing name.whatever
+import <pic> from path
