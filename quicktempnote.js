c0d9b12d3ebdac541ab3a804c20a3852c6640132
Wade Stevens <wstevens22@protonmail.com>
2019-05-22 07:59:45 -0500

    Update quicktempnote.js

--- quicktempnote.js
+++ quicktempnote.js
@@ -6,3 +6,4 @@ You can call the import function last
 name of the main app should be the same as the name of the app
 look up destructuring. It is easier than doing name.whatever
 import <pic> from path
+
